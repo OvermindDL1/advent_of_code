caa6f914b041a1f8e36b3d0e4245e40cece23bf2
OvermindDL1 <overminddl1@gmail.com>
2021-12-01 13:05:38 -0700

    Added Advent of Code 2021-01

--- src/aoc/year2021/day1.rs
+++ src/aoc/year2021/day1.rs
@@ -1,10 +1,56 @@
+use anyhow::Context as _;
 use clap::Parser;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
-pub struct Day1 {}
+pub struct Day1 {
+	/// The input file of "depths"
+	pub input_file: PathBuf,
+}
 
 impl Day1 {
-	pub fn solve(&self) -> anyhow::Result<()> {
-		anyhow::bail!("Not implemented yet");
+	pub fn run(&self) -> anyhow::Result<()> {
+		let mut nums = Vec::with_capacity(8192);
+		{
+			let mut line = String::with_capacity(16);
+			let mut data = BufReader::new(File::open(&self.input_file)?);
+			while let Ok(len) = data.read_line(&mut line) {
+				if len == 0 {
+					break;
+				}
+				let trimmed = line.trim();
+				if !trimmed.is_empty() {
+					nums.push(
+						trimmed
+							.parse::<usize>()
+							.with_context(|| format!("Invalid line: {:?}", line))?,
+					);
+				}
+				line.clear();
+			}
+		}
+		println!(
+			"Step 1: {}",
+			nums.iter()
+				.tuple_windows()
+				.map(|(a, b)| a < b)
+				.filter(|&x| x)
+				.count()
+		);
+		println!(
+			"Step 2: {}",
+			nums.iter()
+				.tuple_windows()
+				.map(|(a, b, c)| a + b + c)
+				.tuple_windows()
+				.map(|(a, b)| a < b)
+				.filter(|&x| x)
+				.count()
+		);
+
+		Ok(())
 	}
 }
--- src/aoc/year2021/mod.rs
+++ src/aoc/year2021/mod.rs
@@ -4,12 +4,13 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 pub enum Year2021 {
+	/// Advent of Code 2021, Day 1 - Sonar Sweep
 	#[clap(name = "1")]
 	Day1(day1::Day1),
 }
 
 impl Year2021 {
 	pub fn run(&self) -> anyhow::Result<()> {
-		todo!("preparing for Advent of Code 2020 year 2021!")
+		crate::run_days!(self, [Day1])
 	}
 }
