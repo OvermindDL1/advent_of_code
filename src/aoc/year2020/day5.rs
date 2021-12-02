a83407682741660ab2f12023907ec780315684c6
OvermindDL1 <overminddl1@gmail.com>
2021-12-02 11:27:31 -0700

    Added Advent of Code 2020-05

---
+++ src/aoc/year2020/day5.rs
@@ -0,0 +1,76 @@
+use crate::aoc::helpers::*;
+use crate::AocApp;
+use anyhow::Context;
+use clap::Parser;
+use itertools::Itertools;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+#[derive(Debug, Parser)]
+pub struct Day5 {
+	/// The input file to use with the parseable data blank line delimited
+	#[clap(default_value = "inputs/2020/day5.input")]
+	pub input_file: PathBuf,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Seat(u16);
+
+impl FromStr for Seat {
+	type Err = anyhow::Error;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		if s.len() != 10 {
+			anyhow::bail!("Invalid seat length: {}", s.len());
+		}
+		let mut seat = 0u16;
+		for (i, b) in s.as_bytes()[..7].iter().copied().enumerate() {
+			match b {
+				b'F' => (),
+				b'B' => seat |= 1 << (9 - i),
+				_ => anyhow::bail!("Invalid row letter: {}", b as char),
+			}
+		}
+		for (i, b) in &mut s.as_bytes()[7..].iter().copied().enumerate() {
+			match b {
+				b'L' => (),
+				b'R' => seat |= 1 << (2 - i),
+				_ => anyhow::bail!("Invalid seat letter: {}", b as char),
+			}
+		}
+
+		Ok(Seat(seat))
+	}
+}
+
+impl Day5 {
+	pub fn run(&self, _app: &AocApp) -> anyhow::Result<()> {
+		let mut highest = Seat(0);
+		let mut seats = map_trimmed_nonempty_lines_of_file(&self.input_file, |line| {
+			let seat = line.parse::<Seat>()?;
+			if seat.0 > highest.0 {
+				highest = seat;
+			}
+			Ok(seat)
+		})?;
+		seats.sort_unstable();
+		println!("Step 1: {}", highest.0);
+		println!(
+			"Step 2: {}",
+			seats
+				.iter()
+				.map(|s| s.0)
+				.tuple_windows()
+				.find_map(|(a, b)| {
+					if b - a > 1 {
+						Some(a + 1)
+					} else {
+						None
+					}
+				})
+				.context("did not find a missing seat")?
+		);
+
+		Ok(())
+	}
+}
--- src/aoc/year2020/mod.rs
+++ src/aoc/year2020/mod.rs
@@ -2,6 +2,7 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
 
 use crate::AocApp;
 use clap::Parser;
@@ -22,14 +23,17 @@ pub enum Year2020 {
 	/// Advent of Code 2020, Day 4 - Passport Processing
 	#[clap(name = "4")]
 	Day4(day4::Day4),
+	/// Advent of Code 2020, Day 5 - Binary Boarding
+	#[clap(name = "5")]
+	Day5(day5::Day5),
 }
 
 impl Year2020 {
 	pub fn run(&self, app: &AocApp) -> anyhow::Result<()> {
-		crate::run_days!(Year2020, self, app, [Day1, Day2, Day3, Day4])
+		crate::run_days!(Year2020, self, app, [Day1, Day2, Day3, Day4, Day5])
 	}
 
 	pub fn run_all(app: &AocApp) -> anyhow::Result<()> {
-		crate::run_all_days!(Year2020, app, [Day1, Day2, Day3, Day4])
+		crate::run_all_days!(Year2020, app, [Day1, Day2, Day3, Day4, Day5])
 	}
 }
