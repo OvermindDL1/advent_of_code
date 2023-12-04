72d2bca014606fd6dd82fa8362b932d6ca84c442
OvermindDL1 <overminddl1@gmail.com>
2023-12-04 10:56:38 -0700

    2023-02 complete

--- src/aoc/year2023/day2.rs
+++ src/aoc/year2023/day2.rs
@@ -1,20 +1,80 @@
 use crate::aoc::helpers::*;
 use crate::AocApp;
+use anyhow::{bail, Context};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
 pub struct Day2 {
-	/// The input file of ""
+	/// The input file of "Dice counts"
 	#[clap(default_value_t = DataFrom::Internal {year: 2023, day: 2})]
 	pub input: DataFrom,
 }
 
 impl Day2 {
 	pub fn run(&self, _app: &AocApp) -> anyhow::Result<()> {
-		// fold_trimmed_nonempty_lines_of_file_bytes
+		// (red, green, blue)
+		let step1_max_color_counts = (12, 13, 14);
+		let mut score1 = 0;
+		let mut score2 = 0;
+		process_trimmed_nonempty_lines_of_file(&self.input, |mut line| {
+			let orig_line = line;
+			line = line
+				.strip_prefix("Game ")
+				.with_context(|| format!("missing `Game ` prefix on line: {orig_line}"))?;
+			let (game_number, line) = line
+				.split_once(": ")
+				.with_context(|| format!("missing `: ` on line: {orig_line}"))?;
+			let game_number = game_number
+				.parse::<u32>()
+				.with_context(|| format!("failed to parse game number on line: {orig_line}"))?;
+			let mut step1_possible = true;
+			let mut max_color_counts = (0, 0, 0);
+			for cubes in line.split("; ") {
+				let mut red = 0;
+				let mut green = 0;
+				let mut blue = 0;
+				for cube in cubes.split(", ") {
+					let (count, color) = cube.split_once(' ').with_context(|| {
+						format!("missing ` ` between count and color on line: {orig_line}")
+					})?;
+					let count =
+						count.parse::<u32>().with_context(|| {
+							format!("failed to parse count `{count}` from `{cube}` on line: {orig_line}")
+						})?;
+					match color {
+						"red" if red == 0 => {
+							red = count;
+							max_color_counts.0 = max_color_counts.0.max(count);
+						}
+						"green" if green == 0 => {
+							green = count;
+							max_color_counts.1 = max_color_counts.1.max(count);
+						}
+						"blue" if blue == 0 => {
+							blue = count;
+							max_color_counts.2 = max_color_counts.2.max(count);
+						}
+						"red" | "green" | "blue" => bail!("duplicate {color} on line: {orig_line}"),
+						_ => bail!("unknown color on line: {orig_line}"),
+					}
+				}
+				if red > step1_max_color_counts.0
+					|| green > step1_max_color_counts.1
+					|| blue > step1_max_color_counts.2
+				{
+					step1_possible = false;
+				}
+			}
 
-		println!("Step 1: {}", -1);
-		println!("Step 2: {}", -1);
+			if step1_possible {
+				score1 += game_number;
+			}
+			score2 += max_color_counts.0 * max_color_counts.1 * max_color_counts.2;
+			Ok(())
+		})?;
+
+		println!("Step 1: {score1}");
+		println!("Step 2: {score2}");
 
 		Ok(())
 	}
--- src/aoc/year2023/mod.rs
+++ src/aoc/year2023/mod.rs
@@ -1,5 +1,6 @@
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 use crate::AocApp;
 use clap::Parser;
@@ -14,23 +15,23 @@ pub enum Year2023 {
 	/// Advent of Code 2023, Day 2: Cube Conundrum
 	#[clap(name = "2")]
 	Day2(day2::Day2),
-	// /// Advent of Code 2023, Day 1: Trebuchet?!
-	// #[clap(name = "3")]
-	// Day3(day3::Day3),
-	// /// Advent of Code 2023, Day 1: Trebuchet?!
+	/// Advent of Code 2023, Day 3: Gear Ratios
+	#[clap(name = "3")]
+	Day3(day3::Day3),
+	// /// Advent of Code 2023, Day 1:
 	// #[clap(name = "4")]
 	// Day4(day4::Day4),
-	// /// Advent of Code 2023, Day 1: Trebuchet?!
+	// /// Advent of Code 2023, Day 1:
 	// #[clap(name = "5")]
 	// Day5(day5::Day5),
 }
 
 impl Year2023 {
 	pub fn run(&self, app: &AocApp) -> anyhow::Result<()> {
-		crate::run_days!(Year2023, self, app, [Day1, Day2])
+		crate::run_days!(Year2023, self, app, [Day1, Day2, Day3])
 	}
 
 	pub fn run_all(app: &AocApp) -> anyhow::Result<()> {
-		crate::run_all_days!(Year2023, app, [Day1, Day2])
+		crate::run_all_days!(Year2023, app, [Day1, Day2, Day3])
 	}
 }
